852d9405862aeb3ffaaa61bb03bee032451d5dd0
techla2019 <114215025+techla2019@users.noreply.github.com>
2023-11-13 10:19:13 +0300

    Add files via upload

---
+++ nav.js
@@ -0,0 +1,11 @@
+const nvv= document.getElementsByClassName('six')[0]
+const navbar = document.getElementsByClassName('four')[0]
+nvv.addEventListener('click', ()=>{
+    navbar.classList.toggle('active')
+})
+const kab= document.getElementsByClassName('six')[0]
+const vbar = document.getElementsByClassName('eight')[0]
+kab.addEventListener('click', ()=>{
+    vbar.classList.toggle('active')
+})
+
